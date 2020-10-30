3a3c08b8b2ba18c928f2869f65e58f23b1ff85cd
351642983 <48375884+351642983@users.noreply.github.com>
2020-10-30 11:02:53 +0800

    语言兼容
    提供socket调用支持，该版本可以在python利用socket发布接口，以便其他语言能调用拟合函数

---
+++ src/socket调用版本/funcmodel.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2020/7/22 22:14
+#@Author: hdq
+#@File  : funcmodel.py
+
+import numpy as np
+import scipy.stats as stats
+
+#自动型最大幂次,减小计算量
+maxn=20
+
+#获得ndarray的从小到大的百分之多少的数
+def get_npnum(x,inprecent):
+    sortlist=np.sort(x)
+    mnum=np.percentile(sortlist,inprecent)
+    if len(np.where(x==mnum)[0])!=0:
+        return mnum
+    else:
+        return sortlist[np.max(np.where(sortlist<=mnum))]
+
+#自动多项式函数拟合(计算量相对较大) pointer:get_npnum
+def auto_ax_bfit(x,y,inpercent=100):
+    x=np.array(x)
+    y=np.array(y)
+    n=maxn if len(x)-1>maxn else len(x)-1
+    alist=[]
+    g_start=True
+    suit=1
+    minsuit=0
+    import warnings
+    warnings.filterwarnings("ignore")
+    #仅仅适用递增数列
+    #np.percentile(a,50) #中位数就是50%处的数字，也可以获得0%、100%处的数字
+    pi=np.where(x==get_npnum(x,inpercent))[0][0]
+    new_x = np.delete(x,pi)
+    new_y = np.delete(y,pi)
+    for i in range(1, n, 1):
+        fit_coef, blist = ax_bfit(new_x, new_y, i)
+        temp_minus = abs(func_general(fit_coef, [x[pi]])[0] - y[pi])
+        if (g_start or temp_minus < minsuit):
+            minsuit = temp_minus
+            suit = i
+            if (g_start):
+                g_start = False
+    warnings.filterwarnings("default")
+    fit_coef, blist = ax_bfit(x, y, suit)
+    a, b = stats.pearsonr(func_general(fit_coef, x), y)
+    score = a * (1 - b)
+    print("自适应最高幂次:",suit)
+    minnum=1e-12
+    for one in fit_coef:
+        if(not g_start):
+            if(abs(one)>minnum):
+                g_start=True
+                alist.append(one)
+        else:
+            alist.append(one)
+    alist=[round(one,9) for one in list(alist)]
+    return fit_coef,alist,score
+
+#靠猜最高幂次多项式拟合，n是最高幂次，这个需要靠猜n的大小，n越大计算量也就越大，同时也越准确（泰勒展开公式，最高幂次越大越逼近准确函数），同时n需要小于len(x),当太大时直接取正整数型，想方便时也直接用自动型就好了
+#但如果一个函数中包含幂函数，则需要将最高幂次调整至对应的范围使其符合幂函数的增长趋势才能让拟合函数在一定区间内符合函数增长，这点自动型做不到
+def ax_bfit(x,y,n):
+    x=np.array(x)
+    y=np.array(y)
+    fit_coef=np.polyfit(x,y,n)
+    alist=[round(one,9) for one in list(np.polyfit(x,y,n))]
+    return fit_coef,alist
+
+#函数构建器
+def func_general(fit_coef,predict_xlist):
+    calculate_y = [np.round(one, 9).tolist() for one in list(np.polyval(fit_coef, predict_xlist))]
+    return calculate_y
+
+
+
+#数据关联性分析 获得相关系数和显著水平
+def get_data_relation(x,y):
+    return stats.pearsonr(x,y)
+
+
+
+#数列规律寻找(数列中包含的None表示该位置没有数值标志，数列规律受数列顺序影响)
+def auto_find_logical(y,inpercent=100):
+    x=[]
+    x_None=[]
+    y_temp=[]
+    for i,info in enumerate(y):
+        if(info!=None):
+            x.append(i+1)
+            y_temp.append(info)
+        else:
+            x_None.append(i)
+    suit=1
+    minsuit=0
+    g_start=True
+    import warnings
+    warnings.filterwarnings("ignore")
+    pi = np.where(x == get_npnum(x, inpercent))[0][0]
+    x_new=np.delete(x,pi)
+    y_new=np.delete(y_temp,pi)
+    for i in range(1,maxn,1):
+        fit_coef, alist =ax_bfit(x_new, y_new, i)
+        temp_minus=abs(func_general(fit_coef, [x[pi]])[0]-y_temp[pi])
+        if(g_start or temp_minus<minsuit):
+            minsuit=temp_minus
+            suit=i
+            if(g_start):
+                g_start=False
+        else:
+            break
+    warnings.filterwarnings("default")
+    fit_coef, alist = ax_bfit(x, y_temp, suit)
+    a, b = stats.pearsonr(func_general(fit_coef,x), y_temp)
+    score = a * (1 - b)
+    print("自适应最高幂次:",suit)
+    for one in x_None:
+        x.insert(one,None)
+    return fit_coef,alist,x,score
+
+#靠猜最高幂次型数列规律寻找(数列中包含的None表示该位置没有数值标志，数列规律受数列顺序影响)
+def find_logical(y,n):
+    x=[]
+    x_None=[]
+    y_temp=[]
+    for i,info in enumerate(y):
+        if(info!=None):
+            x.append(i+1)
+            y_temp.append(info)
+        else:
+            x_None.append(i)
+    fit_coef,alist=ax_bfit(x,y_temp,n)
+    for one in x_None:
+        x.insert(one,None)
+    return fit_coef,alist,x
+
+
+import matplotlib.pyplot as plt
+#展示函数图像
+def show_func(fit_coef,x_min,x_max,exactvalue=201,info=None,show=True):
+    np.set_printoptions(suppress=True)
+    x = np.linspace(x_min, x_max, exactvalue)
+    y = func_general(fit_coef, x)
+    plt.plot(x, y, "r", lw=4, label="predict")
+    if info:
+        plt.plot([one for one in info[-1] if one!=None], [one for one in info[-2] if one!=None], "lightgreen", label="real", lw=2)
+    plt.legend()
+    if(show):
+        plt.show()
+    return plt
+
+#------------函数匹配
+# #数据列表
+# x=[1,2,3,4,5]
+# y=[4,21,64,145,276]
+# #需要预测的x值
+# calculate_x=[1,3,6]
+# #期待值[4,64,469]
+# # #拟合函数最高次幂(这部分可以靠猜，当为3时是准确值,计算量相对较小型)
+# # n=3
+# # fit_coef,alist=ax_bfit(x,y,n)
+#
+# #自动型，计算量相对较大
+# fit_coef,alist,score=auto_ax_bfit(x,y)
+#
+# print("系数列表:",alist)
+# print("calculate_x列表对应的预测值:",func_general(fit_coef,calculate_x))
+
+
+
+# #数据列表
+# x=[2,2.11,3.24,3,4,5,2.1,4.2,3.2,1.3,1.36,1.6,2.7,5.7,3.6]
+# y=[np.exp(one) for one in x]
+#
+# #需要预测的x值
+# calculate_x=[1]
+# #期待值[2.7182818284590452353602874713526624977572]
+# # #拟合函数最高次幂(这部分可以靠猜，当为3时是准确值,计算量相对较小型)
+# # n=3
+# # fit_coef,alist=ax_bfit(x,y,n)
+#
+# #自动型，计算量相对较大
+# fit_coef,alist,score=auto_ax_bfit(x,y)
+#
+# print("系数列表:",alist)
+# print("calculate_x列表对应的预测值:",func_general(fit_coef,calculate_x))
+
+
+
+# #------------数列规律寻找
+# fit_coef,alist,x,score=auto_find_logical([1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765])
+# print(func_general(fit_coef,[x[-1]+1,x[-1]+2]))
+#
+# fit_coef,alist,x,score=auto_find_logical([100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99])
+# print(func_general(fit_coef,[x[-1]+1]))
+
+
+# # ------------相关系数分析
+# x=[1,2,3,4,5]
+# y=[4,21,64,145,276]
+# print(get_data_relation(x,y))
---
+++ src/socket调用版本/funcmodel2.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#@Time  : 2020/7/24 17:19
+#@Author: hdq
+#@File  : funcmodel2.py
+
+import numpy as np
+from scipy.optimize import curve_fit
+import scipy.stats as stats
+
+def func_exxaxx(x,a,b,c,d,e,f,g):
+    x=np.array(x)
+    y = a*np.exp(b*x)+c*x**d+e*x**(f*x)+g
+    return y
+
+
+def func_exxa(x,a,b,c,d,g):
+    x=np.array(x)
+    y = a*np.exp(b*x)+c*x**d+g
+    return y
+
+
+def func_exxx(x,a,b,e,f,g):
+    x=np.array(x)
+    y = a*np.exp(b*x)+e*x**(f*x)+g
+    return y
+
+
+def func_xaxx(x,c,d,e,f,g):
+    x=np.array(x)
+    y = c*x**d+e*x**(f*x)+g
+    return y
+
+def func_ex(x,a,b,g):
+    x=np.array(x)
+    y = a*np.exp(b*x)+g
+    return y
+
+def func_xa(x,c,d,g):
+    x=np.array(x)
+    y = c*x**d+g
+    return y
+
+def func_xx(x,e,f,g):
+    x=np.array(x)
+    y =e*x**(f*x)+g
+    return y
+
+
+import math
+
+#简单周期函数
+def func_sinx(x,a,b,c,d,g):
+    x = np.array(x)
+    y = a*np.sin(b*x*math.pi+c)+d*x+g
+    return y
+
+#高斯曲线拟合(正态函数)
+def func_gauss(x, a, b, c, sigma):
+    x = np.array(x)
+    return a*np.exp(-(x-b)**2/(2*sigma**2)) + c
+
+#傅里叶级数(周期函数拟合)
+def func_fourier(x,*b):
+    x=np.array(x)
+    global t
+    w = 2 * math.pi/(t*2)*b[-1]
+    ret = 0
+    a=b[:-1]
+    for deg in range(0, int(len(a) / 2) + 1):
+        ret += a[deg] * np.cos(deg * w * x) + a[len(a) - deg - 1] * np.sin(deg * w * x)
+    return ret
+
+
+allfunc=["func_exxaxx","func_exxa","func_exxx","func_xaxx","func_ex","func_xa","func_xx","func_sinx","func_gauss","func_fourier"]
+
+def polyfit(x, y,limitfunc=lambda x:x**3):
+    fit_coef_list, pcov_list,scorelist=[],[],[]
+    maxnh=-1
+    id=0
+    suit=0
+    suiti=0
+    import warnings
+    warnings.filterwarnings("ignore")
+    for i,one in enumerate(allfunc):
+        one=eval(one)
+        try:
+            fit_coef, pcov = curve_fit(one, x, y ,maxfev=15000)
+            fit_coef_list.append(fit_coef)
+            pcov_list.append(pcov)
+            result=one([x],*fit_coef)[0]
+            finalone=one([len(x)+1],*fit_coef)[0]
+            downs=0.5 if abs(finalone-result[-1])>limitfunc(max(result)-min(result)) else 0
+            a,b=stats.pearsonr(result, y)
+            score=a*(1-b)-downs
+
+            scorelist.append(score)
+            id+=1
+            if(score>maxnh):
+                maxnh=score
+                suit=id-1
+                suiti=i
+        except Exception as e:
+            print(e)
+            if one == func_fourier:
+                try:
+                    global t
+                    ay=np.array(y)
+                    t=abs(np.where(ay==max(y))[0][0]-np.where(ay==min(y))[0][0])
+                    fit_coef, pcov = curve_fit(one, x, y,[1.0]*(80 if len(x)-2>80 else len(x)-2), maxfev=15000)
+                    fit_coef_list.append(fit_coef)
+                    pcov_list.append(pcov)
+                    result = one([x], *fit_coef)[0]
+                    finalone = one([len(x) + 1], *fit_coef)[0]
+                    downs = 0.5 if abs(finalone - result[-1]) > limitfunc(max(result) - min(result)) else 0
+                    a, b = stats.pearsonr(result, y)
+                    score = a * (1 - b) - downs
+                    scorelist.append(score)
+                    id += 1
+                    if (score > maxnh):
+                        maxnh = score
+                        suit = id - 1
+                        suiti = i
+                except Exception as w:
+                    print(w)
+                    pass
+            pass
+        # print(one, scorelist)
+    warnings.filterwarnings("default")
+    # print(allfunc[suiti])
+    if(len(scorelist)==0):
+        return 0,0,-1
+    #参数
+    return fit_coef_list[suit],allfunc[suiti],scorelist[suit]
+
+def find_logical(y,limitfunc=lambda x:x**3):
+    x_None=[]
+    x=[]
+    y_temp=[]
+    for i,info in enumerate(y):
+        if(info!=None):
+            x.append(i+1)
+            y_temp.append(info)
+        else:
+            x_None.append(i)
+    fit_coef,func,score = polyfit(x, y_temp,limitfunc)
+    for one in x_None:
+        x.insert(one,None)
+    return fit_coef,x,func,score
+
+import matplotlib.pyplot as plt
+#展示函数图像
+def show_func(fit_coef,func,x_min,x_max,exactvalue=201,info=None,show=True):
+    np.set_printoptions(suppress=True)
+    x = np.linspace(x_min, x_max, exactvalue)
+    y = eval(func)(x,*fit_coef)
+    plt.plot(x, y, "r",label="predict", lw=4)
+    if info:
+        # print(info[-1],info[-2])
+        plt.plot([one for one in info[-1] if one!=None], [one for one in info[-2] if one!=None], "lightgreen",label="real", lw=2)
+    plt.legend()
+    if(show):
+        plt.show()
+    return plt
+
+#数据关联性分析 获得相关系数和显著水平
+def get_data_relation(x,y):
+    return stats.pearsonr(x,y)
+
+# #函数对应
+# y=[4,21,64,145,276]
+# x=list(range(1,len(y)+1,1))
+# fit_coef,func,score = polyfit(x, y)
+# print(func([6],*fit_coef))
+#
+#
+#
+# # 数列规律(找下一项是什么)
+# fit_coef,x,func,score=find_logical([2 ,5 ,11 ,23 ,None,95],limitfunc=lambda x:x**3)
+# print(func([5,len(x)+1],*fit_coef))
